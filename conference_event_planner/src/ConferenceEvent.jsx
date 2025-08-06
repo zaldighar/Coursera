ef9bd905375eab7e49db1f4d443d83f56ec84fab
zaldighar <zaldighar@outlook.com>
2025-08-06 15:00:25 -0500

    b

--- conference_event_planner/src/ConferenceEvent.jsx
+++ conference_event_planner/src/ConferenceEvent.jsx
@@ -76,7 +76,41 @@ const getItemsFromTotalCost = () => {
     return items;
   };
   const items = getItemsFromTotalCost();
-  const ItemsDisplay = ({ items }) => {};
+const ItemsDisplay = ({ items }) => {
+    console.log(items);
+    return <>
+        <div className="display_box1">
+            {items.length === 0 && <p>No items selected</p>}
+            <table className="table_item_data">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Unit Cost</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>${item.cost}</td>
+                            <td>
+                                {item.type === "meals" || item.numberOfPeople
+                                ? ` For ${numberOfPeople} people`
+                                : item.quantity}
+                            </td>
+                            <td>{item.type === "meals" || item.numberOfPeople
+                                ? `${item.cost * numberOfPeople}`
+                                : `${item.cost * item.quantity}`}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
+};
   const calculateTotalCost = (section) => {
     let totalCost = 0;
     if (section === "venue") {
--- ecommerce_rtk/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from './Components/CartSlice'
-const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    }
-})
-
-export default store
